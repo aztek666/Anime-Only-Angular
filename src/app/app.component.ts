e3e579573b0e3a4e4bdd9203d64a7e4ad4f85cfb
Mathh <89319059+aztek666@users.noreply.github.com>
2021-08-24 11:44:16 -0300

    V1

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'anime-only',
+  templateUrl: './index.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'animeonlyangulardocs';
+}
+
